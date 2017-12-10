b3f8be7227ac1651905c3e61a1775c4cad329eab
tazzaoui <tazzaoui617@gmail.com>
2017-12-10 13:52:34 -0500

    Copy control and getters

--- Deck.cpp
+++ Deck.cpp
@@ -13,8 +13,12 @@ Deck::Deck(){
     this->cards.push_back(Card(i));
 }
 
+Deck::Deck(const Deck&d){
+  this->current_card = d.current_card;
+  this->cards = d.cards;
+}
+
 void Deck::shuffle(){
-  srand(time(0));
   for(int i = 0; i < 52; ++i){
     size_t index = i + (rand() % (52 - i));
     Card temp = this->cards[i];
@@ -36,12 +40,21 @@ bool Deck::more_cards(void) const{
   return this->current_card < 52;
 }
 
+const vector<Card>& Deck::get_cards()const{
+  return this->cards;
+}
+
 ostream& operator<<(ostream& os, const Deck& d){
   for(size_t i = 0; i < d.cards.size(); ++i)
     os << d.cards.at(i).get_card() << " " << d.cards.at(i) << endl;
   return os;
 }
 
-const vector<Card>& Deck::get_cards()const{
-  return this->cards;
+Deck& Deck::operator=(const Deck& d){
+  if(this != &d){
+    this->current_card = d.current_card;
+    this->cards = d.cards;
+  }
+  return *this;
 }
+
--- Deck.hpp
+++ Deck.hpp
@@ -13,11 +13,13 @@ private:
   size_t current_card;
 public:
   Deck(void);
+  Deck(const Deck&);
   void shuffle(void);
   const Card& deal_card(void);
   bool more_cards(void) const;
-  friend ostream& operator<<(ostream&, const Deck&);
   const vector<Card>& get_cards()const;
+  friend ostream& operator<<(ostream&, const Deck&);
+  Deck& operator=(const Deck&);
 };
 
 #endif
