a735ef0c8917674f327aca6712d3136c94dcb0d5
tazzaoui <tazzaoui617@gmail.com>
2017-12-10 14:53:23 -0500

    Added user's ability to draw

--- Poker.cpp
+++ Poker.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "Deck.hpp"
 #include "Card.hpp"
 #include "Hand.hpp"
 #include "MonteCarlo.hpp"
+#include <string>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
+  printf(R"EOF(
+          _____
+         |A .  | _____
+         | /.\ ||A ^  | _____
+         |(_._)|| / \ ||A _  | _____
+         |  |  || \ / || ( ) ||A_ _ |
+         |____V||  .  ||(_'_)||( v )|
+                |____V||  |  || \ / |
+                       |____V||  .  |
+                              |____V|)EOF");
+
+  
   /*
     Set Verbosity
      0 = No debug info (default)
      1 = Show only results of Monte Carlo Simulation
      2 = Maximum verbosity (INCLUDING THE DEALER'S HAND)
   */
+
   const int verbose = (argc > 1 ? atoi(argv[1]) : 0);
   
   Deck d;
@@ -19,7 +37,7 @@ int main(int argc, char* argv[]){
   srand(time(0));
   
   /* Set up the dealer's hand */
-  cout << "Dealing the dealer's hand..." << endl;
+  cout << endl << endl << "Dealing the dealer's hand..." << endl << endl;
   vector<Card> cards;
   for(size_t i = 0; i < 5; ++i )
    cards.push_back(d.deal_card());
@@ -33,7 +51,7 @@ int main(int argc, char* argv[]){
   }
 
   /* Set up the player's hand... */
-  cout << "Dealing your hand..." << endl;
+  cout << "Dealing your hand..." << endl << endl;
   for(size_t i = 0; i < 5; ++i )
     cards.push_back(d.deal_card());
   Hand h2 = Hand(cards.data());
@@ -42,6 +60,49 @@ int main(int argc, char* argv[]){
   if(verbose)
     cout << "Value: " << h2.value() << endl;
 
+  cout << endl << "You can now choose to draw one, two or three " <<
+    "more cards to replace\nthe corresponding number of unneeded"<<
+    "cards in the original hand..." << endl << endl;
+
+  size_t numToDraw = 0;
+  
+  for (;;) {
+    cout << "Please enter a number in the range of [0,3]" 
+       << " representing the number\nof cards you would like to replace: ";
+    if (cin >> numToDraw && numToDraw >= 0 && numToDraw <=3)
+      break; // valid input
+    else {
+      // Invalid input detected
+      cout << "Invalid input!" << endl;
+      cin.clear(); // Flush buffer
+      cin.ignore(INT_MAX, '\n');
+    }
+  }
+  cout << endl;
+  
+  const size_t index = numToDraw;
+  int card_choices[index];
+  
+  
+  for(size_t i = 0; i < index; ++i){
+    for (;;) {
+      cout <<"Please enter a number in the range of [0,4]" 
+       << " representing the index \nof the card you would like to replace: ";
+      if (cin >> card_choices[i] && card_choices[i] >= 0 && card_choices[i] <=4)
+      break; // valid input
+    else {
+      // Invalid input detected
+      cout << "Invalid input!" << endl;
+      cin.clear(); // Flush buffer
+      cin.ignore(INT_MAX, '\n');
+    }
+    }
+    Card temp = d.deal_card(); 
+    h2.replace(temp, h2.get_hand().at(card_choices[i]));
+    cout << endl;
+  }
+  if(numToDraw)
+    cout << "Your new hand is: " << endl << h2 << endl;
   
   /*
     Simulate n games in each confguration 
@@ -58,7 +119,7 @@ int main(int argc, char* argv[]){
    }
   }
   if(verbose == 2)
-    cout << "Dealer's new hand: " << h1 << endl;
+    cout << endl << "Dealer's new hand: " << h1 << endl;
   
   cout << (h1 > h2 ? "The Dealer's" : "Your") << " hand is better!" << endl;
   
