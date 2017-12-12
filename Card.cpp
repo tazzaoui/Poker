a61f31bc31dd2ac18730e65693e4aa0d37488b96
tazzaoui <tazzaoui617@gmail.com>
2017-12-11 19:28:34 -0500

    Spatial optimizations

--- Card.cpp
+++ Card.cpp
@@ -11,14 +11,8 @@ const string Card::faces[13] = {"2","3","4","5", "6", "7", "8", "9",
 Card::Card(int suit, int face)throw(const char*){
   if(suit > 3 || suit < 0 )
     throw "\033[1;31m SUIT OUT OF RANGE \033[0m\n";
-  else
-    this->suit = suit;
-
   if(face > 12 || face < 0 )
     throw "\033[1;31m FACE OUT OF RANGE \033[0m\n";
-  else
-    this->face = face;
-
   this->card = 13*suit + face;
 }
 
@@ -26,29 +20,20 @@ Card::Card(int card) throw(const char*){
   if(card > 51 || card < 0)
     throw "\033[1;31m CARD OUT OF RANGE \033[0m\n";
   this->card = card;
-  this->suit = card/13;
-  this->face = card % 13;
 }
 Card::Card(const Card& card){
-  this->suit = card.suit;
-  this->face = card.face;
   this->card = card.card;
 }
 
 Card& Card::operator=(const Card& rhs){
   if(this != &rhs){
-    this->suit = rhs.suit;
-    this->face = rhs.face;
     this->card = rhs.card;
   }
   return *this;
 }
 
 bool Card::operator==(const Card& rhs){
-  return
-    this->suit == rhs.suit &&
-    this->face == rhs.face
-    && this->card == rhs.card;
+  return this->card == rhs.card;
 }
 ostream& operator<<(ostream& os, const Card& card){
   os << card.to_string();
@@ -56,6 +41,6 @@ ostream& operator<<(ostream& os, const Card& card){
 }
 
 const string Card::to_string(void) const{
-  return faces[this->face] + " of " + suits[this->suit];
+  return faces[this->card/13] + " of " + suits[this->card % 13];
 }
 
--- Card.hpp
+++ Card.hpp
@@ -10,24 +10,22 @@ private:
   const static string suits[4];
   const static string faces[13];
   
-  /*
+  /*************************SUIT****************************
     RANGE: [0-3]
     {0,1,2,3} --> {Clubs, Diamonds, Hearts, Spades}
     suit = card / 13
-  */
-  int suit;
-
-  /*
+  *************************SUIT****************************/
+  
+  /*************************FACE****************************
     RANGE: [0-12]
     {0,1,...9,10,11,12} --> {2,3,...Jack,Queen,King,Ace}
     face = card % 13
-  */
-  int face;
+   *************************FACE****************************/
 
-  /*
+  /*************************CARD****************************
     RANGE: [0-51]
     card = 13(suit) + face
-  */
+  *************************CARD****************************/
   int card;
 public:
   Card(int suit, int face)throw(const char*);
@@ -43,8 +41,8 @@ public:
   bool operator>=(const Card&);
   */
   const string to_string(void) const;
-  int get_suit(void)const{return this->suit;};
-  int get_face(void)const{return this->face;};
+  int get_suit(void)const{return this->card/13;};
+  int get_face(void)const{return this->card%13;};
   int get_card(void)const{return this->card;};
 };
 #endif
